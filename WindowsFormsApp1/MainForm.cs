19382cbc1b1a0958d7d1ca236dee3afbe6dec29a
ricebal <ricebalvids@gmail.com>
2019-02-01 13:28:11 +0100

    Removed image and added rectangle

--- WindowsFormsApp1/MainForm.cs
+++ WindowsFormsApp1/MainForm.cs
@@ -12,25 +12,19 @@ namespace WindowsFormsApp1
 {
     public partial class MainForm : Form
     {
-        Image GroteZwarteDriehoek = Image.FromFile(@"..\..\Resources\GroteZwarteDriehoek.png");
-
         public MainForm()
         {
             InitializeComponent();
-            
+
         }
-        
+
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
             Pen p = new Pen(myBrush);
             Graphics g = e.Graphics;
-            g.DrawLine(p, new Point(100,100), new Point(300, 200));
-            g.DrawLine(p, new Point(300,200), new Point(100,300));
-            g.DrawLine(p, new Point(100,100), new Point(100, 300));
-
-            
-            e.Graphics.DrawImage(GroteZwarteDriehoek, 100, 1, 30, 30);
+            Rectangle rect = new Rectangle(100, 100, 200, 300);
+            g.FillRectangle(myBrush, rect);
 
             myBrush.Dispose();
             g.Dispose();
