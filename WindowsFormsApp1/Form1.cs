e9dd4a659f25f588cecfbd63e0fdecba76595ac7
BartvanMierlo <bartmierlo@gmail.com>
2019-02-01 12:27:17 +0100

    Added EventHandler for painting

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,20 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+            System.Drawing.Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillRectangle(myBrush, new Rectangle(0, 0, 200, 300));
+            myBrush.Dispose();
+            formGraphics.Dispose();
+
+
+        }
+
+
     }
 }
